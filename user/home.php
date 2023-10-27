397350f8290bace321323679a83676a48a34af15
Lorjohn Raña <lorjohn143@gmail.com>
2023-10-27 14:18:46 +0800

    bring back reserve modal

--- user/home.php
+++ user/home.php
@@ -211,6 +211,21 @@ $books = $bookData->getAllBooks();
         </div>
     </div>
 </div>
+<!-- Reserve Modal -->
+<div class="modal fade " id="reserveModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered">
+        <div class="modal-content" >
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">RESERVING CREDENTIALS</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 <!-- Borrow Success Modal -->
