aa2b5a83b1738d6d01c09ad1d68b7a97dbff2087
Lorjohn Raña <lorjohn143@gmail.com>
2023-11-04 05:15:46 +0800

    added admin profile

--- user/home.php
+++ user/home.php
@@ -26,6 +26,7 @@ $books = $bookData->getAllBooks();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" href="../css/user_home.css">
+
 </head>
 <body>
 <nav class="navbar navbar-light bg-light header" id="navbar" >
