1838da323e1a637394b1e0bfcaa38538ee5b4323
Carla Jean Zozobrado <carlajeanzozobrado@gmail.com>
2023-12-05 01:30:24 +0800

    modify add stud input val

--- js/add_student.js
+++ js/add_student.js
@@ -9,8 +9,60 @@ $(document).ready(function() {
 });
 
 function addStudent() {
+    // Validate the form fields before proceeding
+    if (!validateForm()) {
+        return; // Exit if validation fails
+    }
+
+    // Prepare form data
+    var formData = prepareFormData();
+
+    // AJAX request
+    $.ajax({
+        url: '../operations/add_student.php',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function(response) {
+            handleAjaxSuccess(response);
+        },
+        error: function() {
+            handleAjaxError();
+        }
+    });
+}
+
+function validateForm() {
+    // Add validation logic for each required field
+    var requiredFields = [
+        '#addStudentFirstName',
+        '#addStudentLastName',
+        // ... Add other required fields ...
+        '#addStudentPassword'
+    ];
+
+    for (var i = 0; i < requiredFields.length; i++) {
+        var field = $(requiredFields[i]);
+        if (field.val().trim() === '') {
+            showValidationError('Please fill in all required fields.');
+            return false; // Validation failed
+        }
+    }
+
+    // Validate the file input (if applicable)
+    var profileFileInput = $('#addStudentinput-file')[0];
+    if (profileFileInput.files.length === 0) {
+        showValidationError('Please select a profile image.');
+        return false; // Validation failed
+    }
+
+    return true; // Validation succeeded
+}
+
+function prepareFormData() {
     var formData = new FormData();
-    var profileFileInput = $('#addStudentinput-file')[0]; // Consistent naming
+    var profileFileInput = $('#addStudentinput-file')[0];
     if (profileFileInput.files.length > 0) {
         formData.append('profile', profileFileInput.files[0]);
     }
@@ -30,56 +82,60 @@ function addStudent() {
     formData.append('username', $('#addStudentUsername').val());
     formData.append('password', $('#addStudentPassword').val());
 
-    // AJAX request
-    $.ajax({
-        url: '../operations/add_student.php',
-        type: 'POST',
-        data: formData,
-        contentType: false,
-        processData: false,
-        success: function(response) {
-            try {
-                var result = typeof response === 'string' ? JSON.parse(response) : response;
-                if (result.success) {
-                    $("#StudentModal").modal("hide");
-                    Swal.fire({
-                        title: 'ADDED!',
-                        text: 'SUCCESSFULLY ADDED!',
-                        icon: 'success',
-                        customClass: {
-                            popup: 'my-swal-popup',
-                            title: 'swal-title',
-                            content: 'my-swal-content',
-                            confirmButton: 'my-confirm-button'
-                        }
-                    }).then((result) => {
-                        // Check if the user clicked "OK"
-                        if (result.isConfirmed) {
-                            // Reload the page after a short delay
-                            setTimeout(function() {
-                                location.reload();
-                            });
-                        }
-                    });
+    return formData;
+}
 
-                } else {
-                    Swal.fire({
-                        title: 'Error!',
-                        text: 'Failed to add the student. Please try again.',
-                        icon: 'error'
-                    });
-                }
-            } catch (e) {
-                console.error('Failed to parse JSON response:', response);
-                Swal.fire({
-                    title: 'Error!',
-                    text: 'Failed to process the server response.',
-                    icon: 'error'
-                });
-            }
-        },
-        error: function() {
-            alert('AJAX request failed.'); // Consider improving the user experience
+function handleAjaxSuccess(response) {
+    try {
+        var result = typeof response === 'string' ? JSON.parse(response) : response;
+        if (result.success) {
+            handleSuccessConfirmation();
+        } else {
+            showError('Failed to add the student. Please try again.');
+        }
+    } catch (e) {
+        console.error('Failed to parse JSON response:', response);
+        showError('Failed to process the server response.');
+    }
+}
+
+function handleSuccessConfirmation() {
+    $("#StudentModal").modal("hide");
+    Swal.fire({
+        title: 'ADDED!',
+        text: 'SUCCESSFULLY ADDED!',
+        icon: 'success',
+        customClass: {
+            popup: 'my-swal-popup',
+            title: 'swal-title',
+            content: 'my-swal-content',
+            confirmButton: 'my-confirm-button'
+        }
+    }).then((result) => {
+        // Check if the user clicked "OK"
+        if (result.isConfirmed) {
+            // Reload the page after a short delay
+            setTimeout(function() {
+                location.reload();
+            });
         }
     });
 }
+
+function handleAjaxError() {
+    showError('AJAX request failed.'); // Consider improving the user experience
+}
+
+function showError(message) {
+    Swal.fire({
+        title: 'Error!',
+        text: message,
+        icon: 'error'
+    });
+}
+
+function showValidationError(message) {
+    showError(message);
+}
+
+
