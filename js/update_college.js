61b70a0e37c1c589a9d0e81965c69b0c1eceae6d
Carla Jean Zozobrado <carlajeanzozobrado@gmail.com>
2023-12-12 16:54:17 +0800

    modify update course

--- js/update_college.js
+++ js/update_college.js
@@ -79,11 +79,19 @@ function prepareUpdateFormData()     {
 
 function handleAjaxUpdateSuccess(response) {
     console.log(response); // Log the response for debugging
-
     try {
         var result = typeof response === 'string' ? JSON.parse(response) : response;
         if (result.success) {
-            handleSuccessConfirmation('editCollegeModal');
+            Swal.fire({
+                icon: 'success',
+                title: 'UPDATED!',
+                text: 'SUCCESSFULLY UPDATED!.',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                location.reload();
+                // Handle any additional logic after the user clicks "OK"
+                // For example, redirect to another page or perform some action
+            });
         } else {
             showError('Failed to update the college. Please try again.');
         }
@@ -93,4 +101,68 @@ function handleAjaxUpdateSuccess(response) {
     }
 }
 
+
+function validateUpdateForm() {
+    // Trigger form validation manually
+    var form = $('#EditCollegeModal')[0];
+    form.classList.add('was-validated');
+
+    // Add validation logic for each required field in the update form
+    var requiredFields = [
+        '#editCollegeId',
+        '#editCollegeName',
+    ];
+
+    var nameRegex = /^[A-Za-z\s]+$/; // Regex for letters and spaces
+
+    for (var i = 0; i < requiredFields.length; i++) {
+        var fieldId = requiredFields[i];
+        var field = $(fieldId);
+
+        if (field.length === 0) {
+            console.error('Field not found with ID:', fieldId);
+            continue; // Skip to the next iteration if the field is not found
+        }
+
+        if (field.val().trim() === '') {
+            showValidationError(getCustomErrorMessage(fieldId));
+            return false; // Validation failed
+        }
+
+        // Validate college name using regex
+        if (fieldId === '#editCollegeName' && !nameRegex.test(field.val().trim())) {
+            showValidationError(getCustomErrorMessage(fieldId));
+            return false; // Validation failed
+        }
+    }
+
+    return true; // Validation succeeded
+}
+
+function showValidationError(message) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Validation Error',
+        text: message,
+    });
+}
+
+function getCustomErrorMessage(fieldId) {
+    var errorMessage = '';
+
+    switch (fieldId) {
+        case '#editCollegeId':
+            errorMessage = 'Invalid College ID. It should follow the pattern COL-****.';
+            break;
+        case '#editCollegeName':
+            errorMessage = 'Invalid College Name. It should only contain letters and spaces.';
+            break;
+        // Add more cases for other fields as needed
+        default:
+            errorMessage = 'Invalid input for ' + $(fieldId).attr('placeholder') + '.';
+            break;
+    }
+
+    return errorMessage;
+}
 
