a80c28b90587360084eda11e8899b8630b8510fc
Lorjohn Raña <lorjohn143@gmail.com>
2023-12-15 01:47:32 +0800

    path

--- index_admin.php
+++ index_admin.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
 require_once 'db_config/config.php';
-include "operations/authentication.php";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $db = new Database();
-$userAuth = new UserAuthentication($db);
 
 
 
