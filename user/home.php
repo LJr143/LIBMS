41df2e96c96cb43168bdf72ffea340d99857b1ed
Lorjohn Raña <lorjohn143@gmail.com>
2023-12-14 17:58:32 +0800

    adjust path

--- user/home.php
+++ user/home.php
@@ -420,29 +420,6 @@ if (isset($_SESSION['user'])) {
         </div>
     </div>
 
-     <!-- Feedback successfully submitted -->
-     <div class="modal fade " id="successfulFeedbackModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog  modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <form action="" method="post">
-                        <div style="width: 100%; height: 180px; padding: 10px;">
-                        <div style="width: 100%; display: flex; justify-content: center;"><i class="bi bi-chat" style="font-size: 50px; color: #711717;"></i></div>
-                            <div style="font-size: 14px; letter-spacing: 0.2px; width: 100%; color: #711717; display: flex; justify-content: center; margin-top: 30px">
-                                <p style="font-style: italic; font-weight: 700;">THANK YOU FOR YOUR FEEDBACK!</p>
-                            </div>
-                        </div>
-                        <div style="font-size: 12px; width: 100%; display: flex; justify-content: center;">
-                            <button data-bs-dismiss="modal" type="button" style="width: 95px; margin: 0px 20px; font-weight: bold; border-radius: 5px; padding: 8px; color: #711717; background-color: transparent; border: 1px solid #711717">CLOSE</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
     <!-- Book Review Modal -->
     <div class="modal fade " id="bookReviewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog  modal-dialog-centered">
@@ -476,28 +453,6 @@ if (isset($_SESSION['user'])) {
 
             </div>
 
-        </div>
-    </div>
-
-     <!-- Book Review succesfully submitted -->
-     <div class="modal fade " id="succesfulBookReviewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog  modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <form action="" method="post">
-                    <div style="width: 100%; height: 180px; padding: 10px;">
-                        <div style="width: 100%; display: flex; justify-content: center;"><i class="bi bi-chat" style="font-size: 50px; color: #711717;"></i></div>
-                            <div style="font-size: 14px; letter-spacing: 0.2px; width: 100%; color: #711717; display: flex; justify-content: center; margin-top: 30px">
-                                <p style="font-style: italic; font-weight: 700;">BOOK REVIEW SUBMITTED!</p>
-                            </div>
-                        </div>
-                        <div style="font-size: 12px; width: 100%; display: flex; justify-content: center;">
-                            <button data-bs-dismiss="modal" type="button" style="width: 95px; margin: 0px 20px; font-weight: bold; border-radius: 5px; padding: 8px; color: #711717; background-color: transparent; border: 1px solid #711717">CLOSE</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div>
         </div>
     </div>
 
