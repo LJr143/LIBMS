0e7893e48807741ffc7b88d6cd3fcf76b416c9d0
Lorjohn Raña <lorjohn143@gmail.com>
2023-12-15 02:57:42 +0800

    path

--- user/penalties.php
+++ user/penalties.php
@@ -174,7 +174,7 @@ $getAllTransaction = $transactionData->getAllTransactionByUser($userID);
             <div style=" margin-top: 10px">
                 <p style="font-size: 12px; font-weight: bold; font-style: italic">Borrowed Book Status</p>
             </div>
-            <main class="table" style= height: 100vh; overflow: scroll">
+            <main class="table" style= "height: 100vh; overflow: scroll">
                 <section class="table__header">
                     <div class="input-group">
                         <input type="search" placeholder="Search Data...">
